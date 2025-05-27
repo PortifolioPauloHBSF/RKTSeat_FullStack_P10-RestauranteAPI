771afa2877abc35d48e85e6df5064d72ae23fb59
PauloHBSF <paulohbsfilho@gmail.com>
2025-05-27 20:14:08 -0300

    :rocket: get e post de produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,32 @@
-import { AppError } from "@/utils/AppError";
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
+import { z } from "zod";
 
 class ProductController {
     async index(request: Request, response: Response, next: NextFunction): Promise<any> {
         try {
-            return response.json({ message: "OK" });
+            const { name } = request.query
+            const products = await knex<ProductRepository>("products")
+                .select()
+                .whereLike("name", `%${name ?? ""}%`)
+                .orderBy('name')
+            return response.json({ products });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async create(request: Request, response: Response, next: NextFunction): Promise<any> {
+        try {
+            const bodySchema = z.object({
+                name: z.string({ required_error: "Name is required!" }).trim().min(4),
+                price: z.number({ required_error: "Price is required" }).gt(0),
+            });
+            const { name, price } = bodySchema.parse(request.body);
+
+            await knex<ProductRepository>("products").insert({ name, price });
+
+            return response.status(201).json({ name, price });
         } catch (error) {
             next(error);
         }
