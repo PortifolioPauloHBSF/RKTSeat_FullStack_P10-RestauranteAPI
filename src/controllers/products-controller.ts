dd16224babb43212037e38fcdbef8afddb78bd06
PauloHBSF <paulohbsfilho@gmail.com>
2025-05-27 20:39:57 -0300

    :sparkles: adicionando rota update

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { knex } from "@/database/knex";
-import { z } from "zod";
+import { string, z } from "zod";
 
 class ProductController {
     async index(request: Request, response: Response, next: NextFunction): Promise<any> {
         try {
-            const { name } = request.query
+            const { name } = request.query;
             const products = await knex<ProductRepository>("products")
                 .select()
                 .whereLike("name", `%${name ?? ""}%`)
-                .orderBy('name')
+                .orderBy("name");
             return response.json({ products });
         } catch (error) {
             next(error);
@@ -27,6 +27,28 @@ class ProductController {
             await knex<ProductRepository>("products").insert({ name, price });
 
             return response.status(201).json({ name, price });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async update(request: Request, response: Response, next: NextFunction): Promise<any> {
+        try {
+            const id = z
+                .string()
+                .transform((value) => Number(value))
+                .refine((value) => !isNaN(value), { message: "Id must be a integer" })
+                .parse(request.params.id);
+
+            const bodySchema = z.object({
+                name: z.string({ required_error: "Name is required!" }).trim().min(4).optional(),
+                price: z.number({ required_error: "Price is required" }).gt(0).optional(),
+            });
+            const { name, price } = bodySchema.parse(request.body);
+            await knex<ProductRepository>("products")
+                .update({ name, price, updated_at: knex.fn.now() })
+                .where({ id });
+            return response.json({ updated: true });
         } catch (error) {
             next(error);
         }
